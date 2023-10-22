c9312944d7289f2d6abf7337a071bc5638c2c6ab
Hakan Bayazıt Habeş <hakanbayazithabes@gmail.com>
2023-10-22 13:11:03 +0300

    Added : IoC RepositoryContext

---
+++ WebApi/Controllers/BooksController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class BooksController : ControllerBase
+{
+    private readonly RepositoryContext _context;
+
+    public BooksController(RepositoryContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public IActionResult GetAllBooks()
+    {
+        var books = _context.Books.ToList();
+        return Ok(books);
+    }
+}
