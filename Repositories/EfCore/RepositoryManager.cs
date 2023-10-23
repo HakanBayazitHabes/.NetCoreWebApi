26ff1d6bd4c59da2013620b6a9f87414c30c3aad
Hakan Bayazıt Habeş <hakanbayazithabes@gmail.com>
2023-10-23 11:15:51 +0300

    Added : Repository Manager

---
+++ Repositories/EfCore/RepositoryManager.cs
@@ -0,0 +1,19 @@
+﻿using Repositories.Contracts;
+
+namespace Repositories.EfCore;
+
+public class RepositoryManager : IRepositoryManager
+{
+    private readonly RepositoryContext _context;
+    public RepositoryManager(RepositoryContext context)
+    {
+        _context = context;
+    }
+
+    public IBookRepository Book => new BookRepository(_context);
+
+    public void Save()
+    {
+        _context.SaveChanges();
+    }
+}
