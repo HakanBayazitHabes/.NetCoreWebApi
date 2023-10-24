20a8abf4f527fab8f8b42dc34bd6731a8706e9c4
Hakan Bayazıt Habeş <hakanbayazithabes@gmail.com>
2023-10-24 15:55:17 +0300

    Added : NotFound Exception

---
+++ Entities/Exceptions/BookNotFound.cs
@@ -0,0 +1,8 @@
+namespace Entities.Exceptions;
+
+public sealed class BookNotFound : NotFound
+{
+    public BookNotFound(int id) : base($"The book with id : {id} could not found.")
+    {
+    }
+}
