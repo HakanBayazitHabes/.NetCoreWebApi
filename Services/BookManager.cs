02fe23cc472ed4f31d8a348d1a673fa9ff270782
Hakan Bayazıt Habeş <hakanbayazithabes@gmail.com>
2023-10-24 09:48:14 +0300

    Added : Use of nlog architecture

--- Services/BookManager.cs
+++ Services/BookManager.cs
@@ -9,16 +9,16 @@ namespace Services;
 public class BookManager : IBookService
 {
     private readonly IRepositoryManager _manager;
+    private readonly ILoggerService _logger;
 
-    public BookManager(IRepositoryManager manager)
+    public BookManager(IRepositoryManager manager, ILoggerService logger)
     {
         _manager = manager;
+        _logger = logger;
     }
 
     public Book CreateOneBook(Book book)
     {
-        if (book is null)
-            throw new ArgumentNullException(nameof(book));
         _manager.Book.CreateOneBook(book);
         _manager.Save();
         return book;
@@ -28,7 +28,11 @@ public class BookManager : IBookService
     {
         var entity = _manager.Book.GetOneBookById(id, trackChanges);
         if (entity is null)
-            throw new Exception($"Book with id:{id} could not found");
+        {
+            string message = $"Book with id:{id} could not found";
+            _logger.LogInfo(message);
+            throw new Exception(message);
+        }
         _manager.Book.DeleteOneBook(entity);
         _manager.Save();
     }
@@ -47,7 +51,11 @@ public class BookManager : IBookService
     {
         var entity = _manager.Book.GetOneBookById(id, trackChanges);
         if (entity is null)
-            throw new Exception($"Book with id:{id} could not found");
+        {
+            string message = $"Book with id:{id} could not found";
+            _logger.LogInfo(message);
+            throw new Exception(message);
+        }
 
         if (book is null)
         {
