db28473267e213196e00c0bc83ed9cf1648e5ec7
Hakan Bayazıt Habeş <hakanbayazithabes@gmail.com>
2023-10-24 15:37:21 +0300

    Removed : try-catch blocks

--- Presentation/Controllers/BooksController.cs
+++ Presentation/Controllers/BooksController.cs
@@ -18,82 +18,49 @@ public class BooksController : ControllerBase
     [HttpGet]
     public IActionResult GetAllBooks()
     {
-        try
-        {
-            var books = _manager.BookService.GetAllBooks(false);
-            return Ok(books);
-        }
-        catch (Exception e)
-        {
-            throw new Exception(e.Message);
-        }
+        var books = _manager.BookService.GetAllBooks(false);
+        return Ok(books);
     }
 
     [HttpGet("{id}")]
     public IActionResult GetOneBook([FromRoute(Name = "id")] int id)
     {
-        try
+        throw new Exception("!!!!");
+        var book = _manager.BookService.GetOneBookById(id, false);
+        if (book is null)
         {
-            var book = _manager.BookService.GetOneBookById(id, false);
-            if (book is null)
-            {
-                return NotFound();
-            }
-            return Ok(book);
+            return NotFound();
         }
-        catch (Exception e)
-        {
-            throw new Exception(e.Message);
-        }
+        return Ok(book);
     }
 
     [HttpPost]
     public IActionResult CreateOneBook([FromBody] Book book)
     {
-        try
-        {
-            if (book is null)
-                return BadRequest();
+        if (book is null)
+            return BadRequest();
 
-            _manager.BookService.CreateOneBook(book);
-            return StatusCode(201, book);
-        }
-        catch (Exception e)
-        {
-            throw new Exception(e.Message);
-        }
+        _manager.BookService.CreateOneBook(book);
+        return StatusCode(201, book);
     }
 
     [HttpPut("{id}")]
     public IActionResult UpdateOneBook([FromRoute(Name = "id")] int id, [FromBody] Book book)
     {
-        try
-        {
-            if (book is null)
-                return BadRequest(); //400
+        if (book is null)
+            return BadRequest(); //400
 
-            _manager.BookService.UpdateOneBook(id, book, true);
+        _manager.BookService.UpdateOneBook(id, book, true);
 
-            return NoContent(); //204
-        }
-        catch (Exception e)
-        {
-            throw new Exception(e.Message);
-        }
+        return NoContent(); //204
+
     }
 
     [HttpDelete("{id}")]
     public IActionResult DeleteOneBook([FromRoute(Name = "id")] int id)
     {
-        try
-        {
-            _manager.BookService.DeleteOneBook(id, false);
-            return NoContent();
-        }
-        catch (Exception e)
-        {
-            throw new Exception(e.Message);
-        }
+        _manager.BookService.DeleteOneBook(id, false);
+        return NoContent();
     }
 }
 
