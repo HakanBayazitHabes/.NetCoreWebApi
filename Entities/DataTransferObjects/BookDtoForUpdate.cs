dade07493360f2d622b70b6ddc6c3d732955f7ee
Hakan Bayazıt Habeş <hakanbayazithabes@gmail.com>
2023-10-25 19:07:47 +0300

    Added : Record Types

--- Entities/DataTransferObjects/BookDtoForUpdate.cs
+++ Entities/DataTransferObjects/BookDtoForUpdate.cs
@@ -1,4 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Entities.DataTransferObjects;
 
-public record BookDtoForUpdate(int id, String Title, decimal Price);
+public record BookDtoForUpdate : BookDtoForManipulation
+{
+    [Required]
+    public int Id { get; init; }
+}
 
