b4824071c3fc3b4ac047f71b03443d9744868413
Hakan Bayazıt Habeş <hakanbayazithabes@gmail.com>
2023-12-28 19:27:09 +0300

    Added : price range filtering to BookParameters class

--- Entities/RequestFeatures/BookParameters.cs
+++ Entities/RequestFeatures/BookParameters.cs
@@ -2,5 +2,7 @@ namespace Entities.RequestFeatures;
 
 public class BookParameters : RequestParamaters
 {
-    
+    public uint MinPrice { get; set; }
+    public uint MaxPrice { get; set; } = 1000;
+    public bool validPriceRange => MaxPrice > MinPrice;
 }
