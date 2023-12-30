2365210269c3f13706c3f47a604cf2bdc50a0df2
Hakan Bayazıt Habeş <hakanbayazithabes@gmail.com>
2023-12-30 21:08:23 +0300

    Added : OrderQueryBuilder and update BookRepositoryExtensions

--- Repositories/EfCore/Extensions/BookRepositoryExtensions.cs
+++ Repositories/EfCore/Extensions/BookRepositoryExtensions.cs
@@ -31,45 +31,19 @@ namespace Repositories.EfCore.Extensions
         }
 
         public static IQueryable<Book> Sort(this IQueryable<Book> books,
-        string orderByQueryString){
-            if(string.IsNullOrWhiteSpace(orderByQueryString))
+        string orderByQueryString)
+        {
+            if (string.IsNullOrWhiteSpace(orderByQueryString))
                 return books.OrderBy(b => b.Id);
 
-            var orderParams = orderByQueryString.Trim().Split(',');
+            var orderQuery = OrderQueryBuilder.CreateOrderQuery<Book>(orderByQueryString);
 
-            var propertyInfos = typeof(Book)
-            .GetProperties(BindingFlags.Public | BindingFlags.Instance);
-
-            var orderQueryBuilder = new StringBuilder();
-            
-            //title asceding, price descending, id ascending[,]
-            foreach (var param in orderParams)
-            {
-                if(string.IsNullOrWhiteSpace(param))
-                    continue;
-
-                var propertyFromQueryName = param.Split(' ')[0];
-
-                var objectProperty = propertyInfos
-                .FirstOrDefault(pi=>pi.Name.Equals(propertyFromQueryName,
-                StringComparison.InvariantCultureIgnoreCase));
-
-                if(objectProperty is null)
-                    continue;
-
-                var direction = param.EndsWith(" desc") ? "descending" : "ascending";
-
-                orderQueryBuilder.Append($"{objectProperty.Name.ToString()}  {direction},");
-            }
-
-            var orderQuery = orderQueryBuilder.ToString().TrimEnd(',', ' ');
-
-            if(orderQuery is null)
+            if (orderQuery is null)
                 return books.OrderBy(b => b.Id);
 
             return books.OrderBy(orderQuery);
-        
-        }   
-    
+
+        }
+
     }
 }
