14a9cbfce13e855f98eb28c7a1c2dc5408ad33b6
Hakan Bayazıt Habeş <hakanbayazithabes@gmail.com>
2024-01-03 13:08:21 +0300

    Added : API versioning to BooksController and Presentation.csproj

---
+++ Presentation/Controllers/BooksV2Controller.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.Controllers;
+
+[ApiVersion("2.0")]
+[ApiController]
+[Route("api/books")]
+public class BooksV2Controller : ControllerBase
+{
+}
