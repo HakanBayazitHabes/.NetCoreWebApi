ef171de366c8e030fb424f5ab42018bc758cb53d
Hakan Bayazıt Habeş <hakanbayazithabes@gmail.com>
2024-01-03 15:34:57 +0300

    Refactor API route to include versioning

--- Presentation/Controllers/BooksV2Controller.cs
+++ Presentation/Controllers/BooksV2Controller.cs
@@ -5,7 +5,7 @@ namespace Presentation.Controllers;
 
 [ApiVersion("2.0")]
 [ApiController]
-[Route("api/books")]
+[Route("api/{v:apiversion}/books")]
 public class BooksV2Controller : ControllerBase
 {
     private readonly IServiceManager _manager;
@@ -20,7 +20,11 @@ public class BooksV2Controller : ControllerBase
     public async Task<IActionResult> GetAllBooksAsync()
     {
         var books = await _manager.BookService.GetAllBooksAsync(false);
-
-        return Ok(books);
+        var bookV2 = books.Select(b => new
+        {
+            Title = b.Title,
+            Id = b.Id
+        });
+        return Ok(bookV2);
     }
 }
