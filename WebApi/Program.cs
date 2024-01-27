70163c7a5bbd036b722787d1695f42f477f850ff
Hakan Bayazıt Habeş <hakanbayazithabes@gmail.com>
2024-01-27 16:33:38 +0300

    Added : JWT authentication configuration

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -48,9 +48,9 @@ builder.Services.ConfigureHttpCacheHeaders();
 builder.Services.AddMemoryCache();
 builder.Services.ConfigureRateLimitingOptions();
 builder.Services.AddHttpContextAccessor();
-
-builder.Services.AddAuthentication(); //Kullanıcı adı şifre middleware'inı aktifleştirir
 builder.Services.ConfigureIdentity();
+builder.Services.ConfigureJWT(builder.Configuration);
+
 
 
 var app = builder.Build();
