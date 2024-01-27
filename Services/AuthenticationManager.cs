3c35493de13f4439072cc90ec46867909c73c0ad
Hakan Bayazıt Habeş <hakanbayazithabes@gmail.com>
2024-01-27 20:14:38 +0300

    Added : JWT authentication and token generation

--- Services/AuthenticationManager.cs
+++ Services/AuthenticationManager.cs
@@ -1,8 +1,12 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using AutoMapper;
 using Entities.DataTransferObjects;
 using Entities.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using Services.Contracts;
 
 namespace Services;
@@ -14,12 +18,22 @@ public class AuthenticationManager : IAuthenticationService
     private readonly UserManager<User> _userManager;
     private readonly IConfiguration _configuration;
 
+    private User? _user;
+
     public AuthenticationManager(ILoggerService logger, IMapper mapper, UserManager<User> userManager, IConfiguration configuration)
     {
         _logger = logger;
         _mapper = mapper;
         _userManager = userManager;
         _configuration = configuration;
+    }
+
+    public async Task<string> CreateToken()
+    {
+        var signinCreddentials = GetSigningCredentials();
+        var claims = await GetClaims();
+        var tokenOptions = GenerateTokenOptions(signinCreddentials, claims);
+        return new JwtSecurityTokenHandler().WriteToken(tokenOptions);
     }
 
     public async Task<IdentityResult> RegisterUser(UserForRegistrationDto userForRegistrationDto)
@@ -33,5 +47,55 @@ public class AuthenticationManager : IAuthenticationService
             await _userManager.AddToRolesAsync(user, userForRegistrationDto.Roles);
 
         return result;
+    }
+
+    public async Task<bool> ValidateUser(UserForAuthenticationDto userForAuthDto)
+    {
+        _user = await _userManager.FindByNameAsync(userForAuthDto.UserName);
+        var result = (_user != null && await _userManager.CheckPasswordAsync(_user, userForAuthDto.Password));
+        if (!result)
+            _logger.LogWarning($"{nameof(ValidateUser)}: Authentication failed. Wrong user name or password.");
+
+        return result;
+    }
+
+    private SigningCredentials GetSigningCredentials()
+    {
+        var jwtSettings = _configuration.GetSection("JwtSettings");
+        var key = Encoding.UTF8.GetBytes(jwtSettings["secretKey"]);
+        var secret = new SymmetricSecurityKey(key);
+        return new SigningCredentials(secret, SecurityAlgorithms.HmacSha256);
+    }
+
+    private async Task<List<Claim>> GetClaims()
+    {
+        var claims = new List<Claim>
+        {
+            new Claim(ClaimTypes.Name, _user.UserName)
+        };
+
+        var roles = await _userManager.GetRolesAsync(_user);
+
+        foreach (var role in roles)
+        {
+            claims.Add(new Claim(ClaimTypes.Role, role));
+        }
+
+        return claims;
+    }
+
+    private JwtSecurityToken GenerateTokenOptions(SigningCredentials signinCreddentials, List<Claim> claims)
+    {
+        var jwtSettings = _configuration.GetSection("JwtSettings");
+
+        var tokenOptions = new JwtSecurityToken(
+            issuer: jwtSettings["validIssuer"],
+            audience: jwtSettings["validAudience"],
+            claims: claims,
+            expires: DateTime.Now.AddMinutes(Convert.ToDouble(jwtSettings["expires"])),
+            signingCredentials: signinCreddentials
+        );
+
+        return tokenOptions;
     }
 }
