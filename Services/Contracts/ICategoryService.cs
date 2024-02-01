def1c5c4d7fc351a8a0fd6793784f90034a6df63
Hakan Bayazıt Habeş <hakanbayazithabes@gmail.com>
2024-02-01 03:00:57 +0300

    Added : CategoryManager and ICategoryService interfaces

---
+++ Services/Contracts/ICategoryService.cs
@@ -0,0 +1,9 @@
+using Entities.Models;
+
+namespace Services.Contracts;
+
+public interface ICategoryService
+{
+    Task<IEnumerable<Category>> GetAllCategoriesAsync(bool trackChanges);
+    Task<Category> GetOneCategoryByIdAsync(int id, bool trackChanges);
+}
