c24625b1b3456720eceb3677382fcbf4d061af9c
Hakan Bayazıt Habeş <hakanbayazithabes@gmail.com>
2024-02-01 03:09:20 +0300

    Added : CategoryService to IServiceManager

--- Services/ServiceManager.cs
+++ Services/ServiceManager.cs
@@ -11,6 +11,7 @@ namespace Services;
 public class ServiceManager : IServiceManager
 {
     private readonly Lazy<IBookService> _bookService;
+    private readonly Lazy<ICategoryService> _categoryService;
     private readonly Lazy<IAuthenticationService> _authenticationService;
 
     public ServiceManager(IRepositoryManager repositoryManager,
@@ -22,10 +23,13 @@ public class ServiceManager : IServiceManager
     )
     {
         _bookService = new Lazy<IBookService>(() => new BookManager(repositoryManager, logger, mapper, bookLinks));
+        _categoryService = new Lazy<ICategoryService>(() => new CategoryManager(repositoryManager));
         _authenticationService = new Lazy<IAuthenticationService>(() => new AuthenticationManager(logger, mapper, userManager, configuration));
     }
 
     public IBookService BookService => _bookService.Value;
 
     public IAuthenticationService AuthenticationService => _authenticationService.Value; // Concreate haline ulaşırız.
+
+    public ICategoryService CategoryService => _categoryService.Value;
 }
